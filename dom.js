7f6b925ee9b7370dbd0c36e94d185112f82d2b61
victor519-del! <victoroladimeji519@gmail.com>
2023-06-13 21:06:31 +0100

    feat: add a files

---
+++ dom.js
@@ -0,0 +1,29 @@
+const heading = document.querySelector('h1')
+
+heading.innerText = `Dora said
+ "old men plant trees whose shed they will never live to sit under"`
+
+heading.style.color = '#f0f'
+
+const list1 = document.querySelector('li:nth-of-type(2)')
+
+list1.style.background = 'tomato'
+
+const list = document.querySelectorAll('li')
+
+list[0].innerText = 'blue'
+list[1].style.background = 'cyan'
+list[5].innerText = 'Frontend with REact'
+
+
+const zero = document.getElementById('no-code')
+zero.style.letterSpacing = '20px'
+
+const zero1 = document.querySelector('no-code')[0]
+
+zero.style.backgroundColor = 'cyan'
+
+zero1.style.padding = '3em'
+
+const zerocode =document.getElementsByClassName('no-code')
+
---
+++ index.js
@@ -0,0 +1,84 @@
+const li1 = document.querySelector('li:first-of-type')
+li1.innerText = 'ADETUTU'
+
+const li3 = document.querySelector('li:nth-of-type(3)')
+li3.innerText = 'TAYO'
+
+const li5 = document.querySelector('li:nth-of-type(5)')
+li5.innerText = 'VICTOR'
+
+li1.style.backgroundColor
+const list = document.querySelectorAll('li')
+
+const li = document.querySelectorAll('.left li')
+list[1].innerText = 'JIDE'
+list[3].innerText = 'TIFE'
+list[5].innerText = 'JOSHUA'
+
+const rightEven = document.getElementsByClassName('even')
+rightEven[0].textContent = 'DARASIMI'
+rightEven[1].textContent = 'KOLA'
+rightEven[2].textContent = 'BENEDICT'
+
+const leftOdd = document.getElementsByTagName('li')
+leftOdd[6].innerText = 'SOFIAT'
+leftOdd[8].innerText = 'PRECIOUS'
+leftOdd[10].innerText = 'DAMILARE'
+
+const right = document.getElementsByClassName('right')
+right[0].style.backgroundColor = '#12d'
+
+const left = document.getElementsByClassName('left')[0]
+left.style.backgroundColor = '#8ca'
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+c
